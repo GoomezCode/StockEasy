71c97661a8580ce6ca616302040b6647dfaf2c33
goomez <danielgdpaula@gmail.com>
2026-04-05 18:31:26 -0300

    Struct Path spring boot

---
+++ src/test/java/com/web/StockEasy/StockEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.StockEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
